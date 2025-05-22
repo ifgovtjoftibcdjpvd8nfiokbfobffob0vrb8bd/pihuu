7b1f4852e4b4c59da4b0829293fe1de951457927
ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>
2025-05-22 15:05:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,6 +46,10 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') 
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots0e3463')
+
+
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Kittu_update")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KittuSupport")
 
